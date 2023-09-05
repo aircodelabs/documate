fc1608c0953e5767f3989eeff9ac6d0b01945e31
Adcent <adcentury100@gmail.com>
2023-09-05 17:45:48 +0800

    Add icons and feature patter style to home page

--- docs/.vitepress/theme/components/FeaturePattern.vue
+++ docs/.vitepress/theme/components/FeaturePattern.vue
@@ -44,8 +44,8 @@ const style = computed<{ maskImage: string, WebKitMaskImage: string}>(
 </script>
 
 <template>
-  <div class="pointer-events-none">
-    <div class="absolute inset-0 rounded-2xl transition duration-300 [mask-image:linear-gradient(white,transparent)] group-hover:opacity-50">
+  <div class="pointer-events-none" :data-y="pattern.y">
+    <div class="absolute inset-0 rounded-xl transition duration-300 [mask-image:linear-gradient(white,transparent)] group-hover:opacity-50">
       <GridPattern
         :width="72"
         :height="56"
@@ -56,10 +56,10 @@ const style = computed<{ maskImage: string, WebKitMaskImage: string}>(
       />
     </div>
     <div
-      class="absolute inset-0 rounded-2xl bg-gradient-to-r from-[#D7EDEA] to-[#F4FBDF] opacity-0 transition duration-300 group-hover:opacity-100 dark:from-[#202D2E] dark:to-[#303428]"
+      class="absolute inset-0 rounded-xl bg-gradient-to-r from-[#38BDF8]/10 to-[#0EA5E9]/10 opacity-0 transition duration-300 group-hover:opacity-100 dark:from-[#38BDF8]/15 dark:to-[#0EA5E9]/15"
       :style="style"></div>
     <div
-      class="absolute inset-0 rounded-2xl opacity-0 mix-blend-overlay transition duration-300 group-hover:opacity-100"
+      class="absolute inset-0 rounded-xl opacity-0 mix-blend-overlay transition duration-300 group-hover:opacity-100"
       :style="style"
     >
       <GridPattern
@@ -68,7 +68,7 @@ const style = computed<{ maskImage: string, WebKitMaskImage: string}>(
         x="50%"
         :y="pattern.y"
         :squares="pattern.squares"
-        className="absolute inset-x-0 inset-y-[-30%] h-[160%] w-full skew-y-[-18deg] fill-black/50 stroke-black/70 dark:fill-white/2.5 dark:stroke-white/10"
+        class="absolute inset-x-0 inset-y-[-30%] h-[160%] w-full skew-y-[-18deg] fill-black/50 stroke-black/70 dark:fill-white/2.5 dark:stroke-white/10"
       />
     </div>
   </div>
--- docs/.vitepress/theme/components/GridPattern.vue
+++ docs/.vitepress/theme/components/GridPattern.vue
@@ -6,16 +6,18 @@ defineProps<{
   y: string | number
   squares: Array<[x: number, y: number]>
 }>()
+
+const id = `grid-pattern-${Math.random().toString(36).slice(2)}`
 </script>
 
 <template>
   <svg aria-hidden="true">
     <defs>
-      <pattern id="pt" :width="width" :height="height" patternUnits="userSpaceOnUse" :x="x" :y="y">
+      <pattern :id="id" :width="width" :height="height" patternUnits="userSpaceOnUse" :x="x" :y="y">
         <path :d="`M.5 ${height}V.5H${width}`" fill="none" />
       </pattern>
     </defs>
-    <rect width="100%" height="100%" :strokeWidth="0" fill="url(#pt)" />
+    <rect width="100%" height="100%" :strokeWidth="0" :fill="`url(#${id})`" />
     <svg v-if="squares" :x="x" :y="y" class="overflow-visible">
       <rect strokeWidth="0" v-for="(square, index) in squares" :key="`${square[0]}-${square[1]}`" :width="width + 1"
         :height="height + 1" :x="square[0] * width" :y="square[1] * height" />
