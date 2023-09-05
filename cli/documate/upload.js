9988d523fb52386f26a425a9a8494c272b0d7d13
开坦克的贝塔 <k@aircode.io>
2023-09-05 14:54:08 +0800

    Feat/cli (#17)
    * fix: Fix the problem that the content of package.json is overwritten due to incorrect working directory
    
    * sytle: add more error message for generate documate json
    
    * style: optimize upload files process
    
    * chore: 0.0.3

--- cli/documate/upload.js
+++ cli/documate/upload.js
@@ -1,35 +1,45 @@
-const axios = require('axios');
-const { glob } = require("glob");
 const fsPromises = require('fs/promises');
 const path = require('path');
 
+const axios = require('axios');
+const { glob } = require("glob");
+const Spinnies = require('spinnies');
+
 const uploadFiles = async (uploadUrl, files, token = '') => {  
+  const spinnies = new Spinnies();
+
   try {
-    // Only accepts 30 requests per second
-    const chunkSize = 30;
-    for (let startIndex = 0; startIndex < files.length; startIndex += chunkSize) {
-      const startTime = Date.now();
-  
-      console.log('Documate:uploading files');
-      const subArray = files.slice(startIndex, startIndex + chunkSize);
-  
-      const promises = subArray.map((file) => {
-        return axios.post(uploadUrl, {
-          state: 'uploading',
-          content: file.content,
-          fileName: file.fileName,
-        }, {
-          headers: {
-            token,
-          }
-        });
+    const concurrency = 6;
+    let activePromises = [];
+    let uploadedFilesNum = 0;
+    
+    spinnies.add('uploading', {
+      text: `uploading files: 0/${files.length}`
+    });
+    for (const file of files) {
+      const promise = axios.post(uploadUrl, {
+        state: 'uploading',
+        content: file.content,
+        fileName: file.fileName,
+      }, {
+        headers: { token },
       });
-  
-      await Promise.all(promises);
-      const processIndex = startIndex + chunkSize > files.length ? files.length : startIndex + chunkSize;
-      console.log(`Documate:upload files:: ${processIndex}/${files.length}, duration: ${Date.now() - startTime} ms`);
-      await new Promise((resolve) => setTimeout(resolve, 1500));
+
+      activePromises.push(promise);
+
+      if (activePromises.length === concurrency) {
+        await Promise.race(activePromises).then(() => {
+          uploadedFilesNum += 1;
+          spinnies.update('uploading', {
+            text: `uploading files: ${uploadedFilesNum}/${files.length}`
+          });
+          activePromises = activePromises.filter(p => p !== promise);
+        });
+      }
     }
+
+    await Promise.all(activePromises);
+    spinnies.succeed('uploading', { text: 'Uploading Files Success!', successColor: 'greenBright' });
 
     await axios.post(uploadUrl, {
       state: 'finish',
@@ -39,9 +49,9 @@ const uploadFiles = async (uploadUrl, files, token = '') => {
         token,
       }
     });
-    console.log('Documate:uploaded all files');
   } catch (error) {
-    console.error(error);
+    spinnies.fail('uploading', { text: `Uploading Files Failed Because ${error.message}!`, successColor: 'redBright' });
+
     await axios.post(uploadUrl, {
       state: 'finish',
       mode: 'clear',
@@ -81,8 +91,8 @@ const readFiles = async (config = {}) => {
 };
 
 const upload = async (config = {}) => {
-  const files = await readFiles(options);
-  await uploadFiles(options.backend, files, config.token);
+  const files = await readFiles(config);
+  await uploadFiles(config.backend, files, config.token);
 };
 
 module.exports = upload;
