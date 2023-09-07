3dc712ac420dbe9681f1c36630f326ee518cc4f6
Adcent <adcentury100@gmail.com>
2023-09-07 18:01:11 +0800

    refactor: change package name

--- cli/documate/upload.js
+++ cli/documate/upload.js
@@ -5,61 +5,68 @@ const axios = require('axios');
 const { glob } = require("glob");
 const Spinnies = require('spinnies');
 
-const uploadFiles = async (uploadUrl, files, token = '') => {  
+const handleError = (spinnies, name, error) => {
+  let message = '';
+  if (error && error.response && error.response.data) {
+    message = error.response.data.error;
+  } else {
+    message = error.message;
+  }
+  spinnies.fail(name, { text: `Failed: ${message}`, successColor: 'redBright' });
+  return;
+}
+
+const uploadFiles = async (uploadUrl, files) => {  
   const spinnies = new Spinnies();
 
+  spinnies.add('preparing', {
+    text: 'Preparing...',
+  });
   try {
-    const concurrency = 6;
-    let activePromises = [];
-    let uploadedFilesNum = 0;
-    
-    spinnies.add('uploading', {
-      text: `uploading files: 0/${files.length}`
+    // Currently, we refresh the knowledge base every time.
+    await axios.post(uploadUrl, {
+      operation: 'clean',
     });
+
+    spinnies.succeed('preparing', { text: 'OK. Start uploading.' });
+  } catch (error) {
+    return handleError(spinnies, 'preparing', error);
+  }
+  
+  spinnies.add('uploading', {
+    text: `Uploading files: 0/${files.length}`
+  });
+  try {
+    let uploadedFilesNum = 0;  
+
     for (const file of files) {
-      const promise = axios.post(uploadUrl, {
-        state: 'uploading',
+      await axios.post(uploadUrl, {
+        operation: 'add',
+        path: file.fileName,
         content: file.content,
-        fileName: file.fileName,
-      }, {
-        headers: { token },
       });
 
-      activePromises.push(promise);
-
-      if (activePromises.length === concurrency) {
-        await Promise.race(activePromises).then(() => {
-          uploadedFilesNum += 1;
-          spinnies.update('uploading', {
-            text: `uploading files: ${uploadedFilesNum}/${files.length}`
-          });
-          activePromises = activePromises.filter(p => p !== promise);
-        });
-      }
+      uploadedFilesNum += 1;
+      spinnies.update('uploading', {
+        text: `Uploading files: ${uploadedFilesNum}/${files.length}`
+      });
     }
 
-    await Promise.all(activePromises);
-    spinnies.succeed('uploading', { text: 'Uploading Files Success!', successColor: 'greenBright' });
-
-    await axios.post(uploadUrl, {
-      state: 'finish',
-      mode: 'all',
-    }, {
-      headers: {
-        token,
-      }
-    });
+    spinnies.succeed('uploading', { text: 'Uploading files success.' });
   } catch (error) {
-    spinnies.fail('uploading', { text: `Uploading Files Failed Because ${error.message}!`, successColor: 'redBright' });
-
+    return handleError(spinnies, 'uploading', error);
+  }
+  
+  spinnies.add('generating', {
+    text: 'Generating knowledge base...'
+  });
+  try {
     await axios.post(uploadUrl, {
-      state: 'finish',
-      mode: 'clear',
-    }, {
-      headers: {
-        token,
-      }
+      operation: 'generate',
     });
+    spinnies.succeed('generating', { text: 'Knowledge base generated. Now you can try to Ask AI in you doc site.' });
+  } catch (error) {
+    return handleError(spinnies, 'generating', error);
   }
 };
 
