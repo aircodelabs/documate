c2bbd0f9102366f67de01346241f788fda91b451
akira-cn <akira.cn@gmail.com>
2023-09-21 13:39:16 +0800

    fix: highlight css

--- ui/vanilla-js/src/inject-css.js
+++ ui/vanilla-js/src/inject-css.js
@@ -3,9 +3,10 @@ import highlight from './highlight-js.css';
 
 const headEl = document.head || document.getElementsByTagName('head')[0];
 const styleEl = document.createElement('style');
+const styleText = `${style}\n${highlight}`;
 if(styleEl.styleSheet) {
-  styleEl.styleSheet.cssText = `${style}\n${highlight}`;
+  styleEl.styleSheet.cssText = styleText;
 } else {
-  styleEl.appendChild(document.createTextNode(style));
+  styleEl.appendChild(document.createTextNode(styleText));
 }
 headEl.appendChild(styleEl);
