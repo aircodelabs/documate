47b02074f710efe861dac0038d9f8d512b33189e
akira-cn <akira.cn@gmail.com>
2023-09-21 14:09:02 +0800

    update vanilla-js

--- ui/vanilla-js/src/inject-css.js
+++ ui/vanilla-js/src/inject-css.js
@@ -1,5 +1,5 @@
-import style from './styles.css';
-import highlight from './highlight-js.css';
+import style from './styles/styles.css';
+import highlight from './styles/highlight-js.css';
 
 const headEl = document.head || document.getElementsByTagName('head')[0];
 const styleEl = document.createElement('style');
